c610db9aa03883709686067aa279d1ada6ecbd62
RajanGadhavi <124480232+RajanGadhavi@users.noreply.github.com>
2025-11-11 19:48:40 +0530

    Fix clone path for sepolicy-vndr repository

--- clone_dt.sh
+++ clone_dt.sh
@@ -28,7 +28,7 @@ git clone https://gitlab.com/nothing-2a/proprietary_vendor_nothing_PacmanPro.git
 # ------------------------------
 # Hardware trees
 # ------------------------------
-git clone https://github.com/Nothing-2A/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy-vndr
+git clone https://github.com/Nothing-2A/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 git clone https://github.com/Nothing-2A/android_hardware_mediatek hardware/mediatek
 git clone https://github.com/Nothing-2A/android_hardware_nothing hardware/nothing
 
