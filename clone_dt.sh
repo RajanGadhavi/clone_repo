7985ec099c0c8672b944f68630562b5fd331ce9c
RajanGadhavi <124480232+RajanGadhavi@users.noreply.github.com>
2025-11-11 16:49:46 +0530

    Update working directory to InfinityXA15

--- clone_dt.sh
+++ clone_dt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Automatically set working directory based on where the script is run
-WORKDIR="$PWD/InfinityX"
+WORKDIR="$PWD/InfinityXA15"
 
 # Create the folder if it doesn't exist
 mkdir -p "$WORKDIR"
